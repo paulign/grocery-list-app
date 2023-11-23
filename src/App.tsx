98a77fb112d79476d5119ff8cae199ae00554e82
Pavlo Ihnatiev <vo100193ipa@gmail.com>
2023-11-23 16:40:04 +0200

    initial commit - install prettier, eslint, typescript, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <h1 className="text-3xl">Grocery List App</h1>
+    </div>
+  )
+}
+
+export default App
